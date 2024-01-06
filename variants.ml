ab2c66506e2f1fb29b0c41ebfe61db68b941a13a
mikolaszko <mikosodz.lote@gmail.com>
2024-01-06 09:35:10 +0100

    pattern matching on variants

--- variants.ml
+++ variants.ml
@@ -7,8 +7,19 @@ type point = float * float
 type shape = 
   | Circle of {center : point; radius : float}
   | Rectangle of {lower_left : point; upper_right : point}
+  | Point of point
 
 let c1 = Circle {center = (0.,0.); radius = 1.}
 let r1 = Rectangle {lower_left = (1.,5.5); upper_right = (-1., 5.)}
+let p1 = Point (41., 10.)
 
+let avg a b =
+  (a +. b) /. 2.
 
+let center s = 
+  match s with
+  | Circle {center; radius} -> center
+  | Rectangle {lower_left = (x_ll, y_ll);
+               upper_right = (x_ur, y_ur)} -> 
+      (avg x_ll x_ur, avg y_ll y_ur)
+  | Point p -> p
