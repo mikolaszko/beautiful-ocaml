bc9b55bc43a3097e5275a871849c7a724c7b4524
Mikołaj Sodzawiczny <mikolaj@worldanvil.com>
2024-03-06 22:44:40 +0100

    next_benefector problem solved

--- kata-pracitce/look_for_benef.ml
+++ kata-pracitce/look_for_benef.ml
@@ -1,14 +1,12 @@
+(* grog suck at math *)
 let new_avg (xs: float list) (navg: float): int option =
-  let help_counter =
-    let newDonListLen = List.length xs + 1 in
-    let summed = List.fold_left ( +. ) 0.0 xs in
-    print_float summed;
-    Option.some (Float.to_int summed);
-  in
-    
-  match xs with 
-  | [] -> Option.some 5
-  | _ ->
-    match navg <= 0.0 with
-    | true -> None
-    | false -> help_counter
+  let newAccList = List.map (fun x -> navg -. x) xs in
+  let avg = List.fold_left ( +. ) 0.0 xs /. Int.to_float ( List.length xs ) in
+  match avg >= navg with 
+  | false  -> (
+    let neededDon = List.fold_left ( +. ) 0. newAccList +. navg in
+    match xs with 
+    | [] -> Option.some (Float.to_int navg)
+    | _ -> Option.some (Float.to_int (Float.ceil neededDon))
+  )
+  | true -> None
